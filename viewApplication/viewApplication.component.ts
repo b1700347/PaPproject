c69ac5025a905a0871c248d16d19ac8b1ab1fd65
b1700347 <55172195+b1700347@users.noreply.github.com>
2019-10-07 10:56:34 +0800

    Add files via upload

---
+++ viewApplication/viewApplication.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Application } from '../application.model';
+import { ApplicationService } from '../application.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-view-application',
+  templateUrl: './viewApplication.component.html'
+})
+export class ViewAppComponent implements OnInit {
+  user = 'HouseOwner';
+  applications: Application[] = [];
+  private appSub: Subscription;
+
+  // constructor function to do method in service
+  constructor(public AppService: ApplicationService) {
+  }
+  ngOnInit() {
+    this.applications = this.AppService.getPosts();
+    this.appSub = this.AppService.getPostUpdateListener()
+      .subscribe((applications: Application[]) => {
+        this.applications = applications;
+      });
+  }
+  OnDestroy() {
+    this.appSub.unsubscribe();
+  }
+}
