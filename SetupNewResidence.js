0cad1d528c475114a79ece41f96e596093ff9a26
kamilddm <54580073+kamilddm@users.noreply.github.com>
2019-10-07 19:45:11 +0800

    Delete SetupNewResidence.js

--- SetupNewResidence.js
+++
@@ -1,7 +0,0 @@
-var input = document.querySelector('input'),
-      btn = document.querySelector('button');
-
-btn.addEventListener('click', function() {
-  var MIN = 1, MAX = 50;
-  input.value = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
-});
